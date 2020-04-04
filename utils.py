7052aeba1b27f159d7407dc043255aff714ca125
wongxy <xiyao.wong@foxmail.com>
2020-04-04 20:17:12 +0800

    Update func: download

--- utils.py
+++ utils.py
@@ -25,24 +25,26 @@ def check_dir(path):
     return True
 
 
-def download(url, file_name=None, file_type=None, headers=None):
+def download(file_url, file_name=None, file_type=None, save_path="download", headers=None):
     """
-    :param url: 下载资源链接
+    :param file_url: 下载资源链接
     :param file_name: 保存文件名，默认为当前日期时间
-    :param file_type: 文件扩展名，有的话就单独创建文件夹
+    :param file_type: 文件类型(扩展名)，也是保存路径
+    :param save_path: 保存路径，默认为download,后面不要"/"
     :param headers: http请求头，默认为iphone
     """
-    check_dir("download")
-
     if file_name is None:
         file_name = str(datetime.now())
     file_name = filter_name(file_name)
 
-    if file_type is not None:
-        check_dir(f"download/{file_type}")
-        file_name = file_name + "." + file_type
-    else:
-        file_type = "default"
+    if file_type is None:
+        if "." in file_url:
+            file_type = file_url.split(".")[-1]
+        else:
+            file_type = "uknown"
+
+    check_dir(f"{save_path}/{file_type}")
+    file_name = file_name + "." + file_type
 
     if headers is None:
         headers = {
@@ -52,14 +54,14 @@ def download(url, file_name=None, file_type=None, headers=None):
 
     # 下载提示
     print(f"Downloading {file_name}")
-    with requests.get(url, headers=headers, stream=True) as rep:
+    with requests.get(file_url, headers=headers, stream=True) as rep:
         file_size = int(rep.headers['Content-Length'])
         if rep.status_code != 200:
             print("下载失败")
             return False
         label = '{:.2f}MB'.format(file_size / (1024 * 1024))
         with click.progressbar(length=file_size, label=label) as progressbar:
-            with open(f"download/{file_type}/{file_name}", "wb") as f:
+            with open(f"{save_path}/{file_type}/{file_name}", "wb") as f:
                 for chunk in rep.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
