63516446f9e6ce9ac37eb8db51dd360b77105d9a
wongxy <xiyao.wong@foxmail.com>
2020-04-28 18:04:33 +0800

    fix #5 douyin

--- extractor/douyin.py
+++ extractor/douyin.py
@@ -5,48 +5,47 @@ import requests
 
 def get(share_url) -> dict:
     """
-    title、videos
+    author, title, audioName, audios, videoName, videos
     """
     data = {}
     headers = {
-        'accept':
-        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'accept-encoding':
-        'gzip, deflate, br',
-        'accept-language':
-        'zh-CN,zh;q=0.9',
-        'cache-control':
-        'max-age=0',
-        'upgrade-insecure-requests':
-        '1',
-        'user-agent':
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+        'accept': 'application/json',
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
     }
+    api = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={item_id}&dytk={dytk}"
 
-    title_pattern = r'<div class="user-title">(.*?)</div>'
-    playwm_url_pattern = r'<video id="theVideo" class="video-player" src="(.*?)" preload'
+    # get html text
+    rep = requests.get(share_url, headers=headers, timeout=10)
+    if not rep.ok:
+        return {"msg": "获取失败"}
+    html_text = rep.text
 
-    with requests.get(share_url, headers=headers, timeout=10) as rep:
-        share_url = rep.headers.get('location', share_url)
+    # get item_id, dytk
+    item_id = re.findall(r'itemId: "(\d+)"', html_text)
+    dytk = re.findall(r'dytk: "(.*?)"', html_text)
+    if not item_id or not dytk:
+        return {"msg": "获取失败"}
+    item_id = item_id[0]
+    dytk = dytk[0]
 
-    with requests.get(share_url, headers=headers, timeout=10) as rep:
-        html_text = rep.text
+    # get video info
+    rep = requests.get(api.format(item_id=item_id, dytk=dytk), headers=headers, timeout=6)
+    if not rep.ok or not rep.json()["status_code"] == 0:
+        return {"msg": "获取失败"}
+    info = rep.json()["item_list"][0]
 
-    title = re.findall(title_pattern, html_text)
-    if title:
-        title = title[0]
-        data["title"] = title
+    data["author"] = info["author"]["nickname"]
+    data["title"] = data["videoName"] = info["desc"]
+    data["audioName"] = info["music"]["title"]
+    data["audios"] = [info["music"]["play_url"]["uri"]]
+    # data["imgs"] = [info["video"]["origin_cover"]["url_list"][0]]
 
-    playwm_url = re.findall(playwm_url_pattern, html_text)
-    if playwm_url:
-        play_url = playwm_url[0].replace('playwm', 'play')
-    else:
-        data["msg"] = "视频地址获取失败"
-        return data
+    # get playwm_url -> play_url
+    play_url = info["video"]["play_addr"]["url_list"][0].replace('playwm', 'play')
 
-    with requests.get(play_url, headers=headers, allow_redirects=False, timeout=10) as rep:
-        video_url = rep.headers.get('location', '')
-        data["videos"] = [video_url]
+    rep = requests.get(play_url, headers=headers, allow_redirects=False, timeout=6)
+    video_url = rep.headers.get('location', '')
+    data["videos"] = [video_url]
 
     return data
 
