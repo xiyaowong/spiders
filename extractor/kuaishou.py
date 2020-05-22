f3a15256cfb5d90ff2868a5294f26ada6adb8a04
wongxy <xiyao.wong@foxmail.com>
2020-05-22 20:55:05 +0800

    Fix kuaishou(#9)

--- extractor/kuaishou.py
+++ extractor/kuaishou.py
@@ -1,7 +1,7 @@
-# @wongxy
-import re
+import json
 
 import requests
+from lxml import etree
 
 
 def get(url: str) -> dict:
@@ -11,29 +11,37 @@ def get(url: str) -> dict:
     data = {}
     headers = {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25",
-        "Cookie": "did=web_66d3f43c55744795a6210847eff2074e; didv=1585296414000",
+        "Cookie": "did=web_68e0268146694843a92700d2de49a0a6;"
     }
-    re_title = r'<meta name="description" itemprop="description" content="(.*?)">'
-    re_video = r';srcNoMark&#34;:&#34;(https://.*?\.mp4)'
-    re_img = r'&#34;path&#34;:&#34;(.*?\.jpg)&#34;'
-
     rep = requests.get(url, headers=headers, timeout=10)
-    if rep.status_code != 200:
-        return {"msg": f"{rep.status_code}获取失败"}
+    if rep.status_code == 200:
+        tree = etree.HTML(rep.text)  # pylint: disable=c-extension-no-member
 
-    title = re.findall(re_title, rep.text)
-    video = re.findall(re_video, rep.text)
-    img = re.findall(re_img, rep.text)
-    if title:
-        data["title"] = title[0]
-    if video:
-        data["videos"] = video
-# https://js2.a.yximgs.com/ufile/atlas/NTE5Njg3MjUyMDE5MTk4MTM4OF8xNTg0NzkyNzI4MjAy_15.jpg
-    if img:
-        data["imgs"] = ["https://js2.a.yximgs.com" +
-                        i for i in img[:-1]]  # 最后一张是水印
-    return data
+        # title
+        desc = tree.xpath(r"//meta[@name='description']/@content")
+        if desc:
+            data['title'] = desc[0]
+
+        # imgs
+        imgs = tree.xpath(r"//img[@class='play-long-image']/@src")
+        if imgs:
+            data['imgs'] = ["https:" + i for i in imgs]
+
+        # video
+        hide_data = tree.xpath(r"//div[@id='hide-pagedata']/@data-pagedata")
+        if hide_data:
+            try:
+                data_ = json.loads(hide_data[0])
+                data['video'] = [data_['video']['srcNoMark']]
+                data['title'] = data['videoName'] = data_['video']['caption']
+            except Exception:
+                pass
+
+        return data
+
+    return {'msg': 'failed...'}
 
 
 if __name__ == "__main__":
-    print(get(input("url: ")))
+    from pprint import pprint
+    pprint(get(input("url: ")))
